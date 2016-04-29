78b244852e154c95547a4e006745b1ac90722e23
kharism <greyroot00@gmail.com>
2016-04-29 13:11:46 +0700

    example for simplified usage

--- example/main.go
+++ example/main.go
@@ -4,6 +4,7 @@ import(
 	. "hashtree"
 )
 func main(){
+	/*
 	root := make(map[int]*HashNode)
 	root[0] = &HashNode{Key:0}
 	root[0].AddValueRecursive([]int{2,3})
@@ -12,5 +13,8 @@ func main(){
 	root[0].AddValueRecursive([]int{})
 	fmt.Println(root[0].GetValueRecursive([]int{2,4}))
 	fmt.Println(root[0].GetValueRecursive([]int{2,4,13}))
-	fmt.Println(root[0].GetValueRecursive([]int{2,3}))
+	fmt.Println(root[0].GetValueRecursive([]int{2,3}))*/
+	root:=&HashNode{}
+	root.AddValueRecursive([]int{0,2,3})
+	fmt.Println(root.GetValueRecursive([]int{0,2,3}))
 }
